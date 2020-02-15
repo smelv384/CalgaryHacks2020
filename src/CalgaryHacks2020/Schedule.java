46dfd481cd8af6e6c30180b08abd1e45eb2804a1
ManjotKhangura1 <59262828+ManjotKhangura1@users.noreply.github.com>
2020-02-15 16:51:48 -0700

    ...

--- src/CalgaryHacks2020/Schedule.java
+++ src/CalgaryHacks2020/Schedule.java
@@ -3,6 +3,8 @@ package CalgaryHacks2020;
 public class Schedule {
 	
 	private Event[13][7][24] tempSchedule;
+	
+	
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
