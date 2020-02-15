a8094dd7606a9749bab7ef2f30bba957922af13a
samuel Osweiler <32417612+SamuelGarm@users.noreply.github.com>
2020-02-15 13:02:30 -0700

    ADded skeleton
    BLAH

---
+++ src/CalgaryHacks2020/Schedule.java
@@ -0,0 +1,5 @@
+package CalgaryHacks2020;
+
+public class Schedule {
+
+}
---
+++ src/CalgaryHacks2020/Student.java
@@ -0,0 +1,10 @@
+package CalgaryHacks2020;
+
+import java.util.ArrayList;
+
+public class Student {
+	String name;
+	ArrayList<String> intrests = new ArrayList<String>();
+	Schedule schedule;
+	
+}
