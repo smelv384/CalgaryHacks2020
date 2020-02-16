119c6a12c4712c2ed82b87bc9cfb7a2a4d6ff899
smelv384 <'smelv384@mtroyal.ca'>
2020-02-15 23:12:22 -0700

    Fixed setters in Assignment

--- src/CalgaryHacks2020/Assignment.java
+++ src/CalgaryHacks2020/Assignment.java
@@ -1,15 +1,12 @@
 package CalgaryHacks2020;
 
-import java.util.GregorianCalendar;
-import java.util.Scanner;
-
 public class Assignment {
 
     //Setters should probably have their variables processed outside of the method so that
     //the program can integrate easily with a GUI
 
     private String name;
-    private GregorianCalendar date;
+    private int[] date;
     private int hours;
     private String className;
 
@@ -17,50 +14,27 @@ public class Assignment {
         //Should figure this out at some point
     }
 
-	public void setName() {
-		Scanner input = new Scanner(System.in);
-		System.out.print("Enter the name of the assignment: ");
-		this.name = input.next();
-		input.close();
+	public void setName(String name) {
+		this.name = name;
 	}
 
-	public void setDate() {
-		Scanner input = new Scanner(System.in);
-		System.out.println("Please enter the due date.");
-		System.out.print("Enter the due year: ");
-		int year = input.nextInt();
-		System.out.print("Enter the due month: ");
-		int month = input.nextInt();
-		System.out.print("Enter the due day: ");
-		int day = input.nextInt();
-		System.out.print("Enter the due hour: ");
-		int hour = input.nextInt();
-		System.out.print("Enter the due minute: ");
-		int minute = input.nextInt();
-		input.close();
-
-		this.date = new GregorianCalendar(year, month, day, hour, minute);
+	public void setDate(int week, int day, int hour) {
+		date = new int[] { week, day, hour };
 	}
 
-	public void setHours() {
-		Scanner input = new Scanner(System.in);
-		System.out.print("Enter the amount of hours you want to work: ");
-		this.hours = input.nextInt();
-		input.close();
+	public void setHours(int hours) {
+		this.hours = hours;
 	}
 
-	public void setClassName() {
-		Scanner input = new Scanner(System.in);
-		System.out.print("Enter the name of the class: ");
-		this.className = input.next();
-		input.close();
+	public void setClassName(String className) {
+		this.className = className;
 	}
 
 	public String getName() {
 	    return name;
 	}
 
-	public GregorianCalendar getDate() {
+	public int[] getDate() {
 	    return date;
 	}
 
