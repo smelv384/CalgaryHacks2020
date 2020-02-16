326aeb38308425138eb9a54e71861ae2615872de
smelv384 <'smelv384@mtroyal.ca'>
2020-02-15 22:17:28 -0700

    Fixed merging issues

--- src/CalgaryHacks2020/StudentReader.java
+++ src/CalgaryHacks2020/StudentReader.java
@@ -12,25 +12,22 @@ public class StudentReader {
     public StudentReader() {
     }
 
-//    public Student read(String fileName, String scheduleFileName) {
-//        Scanner in = new Scanner(System.in);
-//        try {
-//            File inFile = new File(fileName);
-//            in = new Scanner(inFile);
-//        } catch (FileNotFoundException e) {
-//            System.out.println("This should literally never happen");
-//        }
-//        String studentName = in.nextLine();
-//        Schedule studentSchedule;
-//        ScheduleBuilder studentScheduleBuilder = new ScheduleBuilder();
-//        studentSchedule = studentScheduleBuilder.build(scheduleFileName); //this is made assuming classical naming conventions for the builder method
-//        String studentUcID = in.nextLine();
-//
-//        Student newStudent = new Student(studentName, studentSchedule, studentUcID);
-//        return newStudent;
-//
-//
-//
-//        in.close();
-//    }
+    public Student read(String fileName, String scheduleFileName) {
+        Scanner in = new Scanner(System.in);
+        try {
+            File inFile = new File(fileName);
+            in = new Scanner(inFile);
+        } catch (FileNotFoundException e) {
+            System.out.println("Fatal error");
+        }
+        String studentName = in.nextLine();
+        Schedule studentSchedule;
+        ScheduleBuilder studentScheduleBuilder = new ScheduleBuilder();
+        studentSchedule = studentScheduleBuilder.read(scheduleFileName); //this is made assuming classical naming conventions for the builder method
+        String studentUcID = in.nextLine();
+
+        Student newStudent = new Student(studentName, studentSchedule, studentUcID);
+        in.close();
+        return newStudent;
+    }
 }
