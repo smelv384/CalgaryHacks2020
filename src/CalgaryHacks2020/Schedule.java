882a1ef9c552ca23976ca87f690b21b4939a28ed
ManjotKhangura1 <59262828+ManjotKhangura1@users.noreply.github.com>
2020-02-15 17:32:07 -0700

    Added the empty 3D array and the Set for classes in Schedule class

--- src/CalgaryHacks2020/Schedule.java
+++ src/CalgaryHacks2020/Schedule.java
@@ -1,13 +1,13 @@
 package CalgaryHacks2020;
 
+import java.util.Set;
+
 public class Schedule {
 
-	private Event[13][7][24] tempSchedule;
-	
-	
+	private Event[][][] tempSchedule = new Event[13][7][24];
+	private Set<String> classSet;
 
 	public Schedule() {
-	    Event[13][7][24] tempSchedule = new Event[13][7][24]();
 	    for (int i = 0; i < 13; i++) {
 	        for (int j = 0; j < 7; j++) {
 	            for (int k = 0; k < 24; k++) {
@@ -17,5 +17,20 @@ public class Schedule {
 	    }
 
 	}
+	
+	public Event[][][] getTempSchedule() {
+		return tempSchedule;
+	}
+	public void setTempSchedule(Event[][][] newTempSchedule) {
+		this.tempSchedule = newTempSchedule;
+	}
 
+	public Set<String> getClassSet() {
+		return classSet;
+	}
+
+	public void setClassSet(Set<String> newClassSet) {
+		this.classSet = newClassSet;
+	}
+	
 }
