f0bd4e1d1a924df503b0dbfb29ae79835cd7ecc6
harol <harold.d.lee@gmail.com>
2020-02-15 19:44:24 -0700

    Assignment class finished

--- src/CalgaryHacks2020/Assignment.java
+++ src/CalgaryHacks2020/Assignment.java
@@ -1,14 +1,46 @@
 package CalgaryHacks2020;
 
-import java.util.Date;
+import java.util.GregorianCalendar;
+import java.util.Scanner;
 
 public class Assignment {
-	private String name;
-	private Date dueDate;
-	private Date dueTime;
-	private int hours;
-	private String clas;
 	
+	public String setName() {
+		Scanner input = new Scanner(System.in); 
+		System.out.print("Enter the name of the assignment: ");
+		String name = input.next();
+		return name;	
+	}
 	
+	public GregorianCalendar setDate() {
+		Scanner input = new Scanner(System.in);
+		System.out.println("Please enter the due date.");
+		System.out.print("Enter the due year: ");
+		int year = input.nextInt();
+		System.out.print("Enter the due month: ");
+		int month = input.nextInt();
+		System.out.print("Enter the due day: ");
+		int day = input.nextInt();
+		System.out.print("Enter the due hour: ");
+		int hour = input.nextInt();
+		System.out.print("Enter the due minute: ");
+		int minute = input.nextInt();
+		
+		return new GregorianCalendar(year, month, day, hour, minute);
+	}
+	
+	public int setHours() {
+		Scanner input = new Scanner(System.in);
+		System.out.print("Enter the amount of hours you want to work: ");
+		int hour = input.nextInt();
+		return hour;
+	}
+	
+	public String setClass() {
+		Scanner input = new Scanner(System.in);
+		System.out.print("Enter the name of the class: ");
+		String className = input.next();
+		return className;
+	}
 	
 }
