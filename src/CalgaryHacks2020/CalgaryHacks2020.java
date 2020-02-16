439af991aa0ab88d4f92b22611e388b856279e1c
alexanna-little <55464663+alexanna-little@users.noreply.github.com>
2020-02-16 04:52:52 -0700

    makeACollabInvite is now static

--- src/CalgaryHacks2020/CalgaryHacks2020.java
+++ src/CalgaryHacks2020/CalgaryHacks2020.java
@@ -129,8 +129,7 @@ public class CalgaryHacks2020 {
     }
 
     public static void createCollabRequest(Assignment asgCollab) {
-        CollabInvite collabRequest = new CollabInvite(user, allStudents, asgCollab);
-        ArrayList<ArrayList<Object>> studentsToInvite = collabRequest.makeACollabInvite(user, allStudents, asgCollab);
+        ArrayList<ArrayList<Object>> studentsToInvite = CollabInvite.makeACollabInvite(user, allStudents, asgCollab);
     }
 
     public static void userLookForInvites(ArrayList<ArrayList<Object>> studentsToInvite) {
