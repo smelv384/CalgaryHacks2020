212b47d10b54293dddecd26668aa36703c51f772
harol <harold.d.lee@gmail.com>
2020-02-15 19:42:40 -0700

    Assignment class finished.

--- src/CalgaryHacks2020/Assignment.java
+++ src/CalgaryHacks2020/Assignment.java
@@ -1,5 +1,14 @@
 package CalgaryHacks2020;
 
-public class Assignment {
+import java.util.Date;
 
+public class Assignment {
+	private String name;
+	private Date dueDate;
+	private Date dueTime;
+	private int hours;
+	private String clas;
+	
+	
+	
 }
