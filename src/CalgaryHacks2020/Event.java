b3e3de0ec891348fa75c41162103d99c895313f9
smelv384 <'smelv384@mtroyal.ca'>
2020-02-15 17:45:00 -0700

    Finished event class

--- src/CalgaryHacks2020/Event.java
+++ src/CalgaryHacks2020/Event.java
@@ -1,16 +1,34 @@
+/**
+ * Made by the U of C-MRU Coalition for the Purpose of Victory in CalgaryHacks2020
+ * Author: Shaemus Melvin
+ */
+
 package CalgaryHacks2020;
 
 public class Event {
-    private enum eventType {
+    private enum EventType {
         FREETIME, CLASS, STUDYING
     }
 
+    private EventType eventType;
     Assignment optionalAssignment;
 
     public Event() {
 
     }
 
-    public
+    public void setEventType(int eventInt) {
+        switch (eventInt) {
+            case 0:
+                eventType = EventType.FREETIME;
+                break;
+            case 1:
+                eventType = EventType.CLASS;
+                break;
+            case 2:
+                eventType = EventType.STUDYING;
+                break;
+        }
+    }
 
 }
