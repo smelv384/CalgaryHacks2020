a2a52d42f3a183b1aaedc1efda87343291059647
smelv384 <'smelv384@mtroyal.ca'>
2020-02-15 22:33:02 -0700

    Fixed free time checking

--- src/CalgaryHacks2020/CollabInvite.java
+++ src/CalgaryHacks2020/CollabInvite.java
@@ -1,21 +1,19 @@
 package CalgaryHacks2020;
 import java.util.ArrayList;
-import java.util.Date;
 import java.util.GregorianCalendar;
 import java.util.Set;
-import CalgaryHacks2020.Event.EventType;
 
 public class CollabInvite {
-	
-	
+
+
 	private Student inviter;
 	private ArrayList<Student> possibleCollaborators;
 	private Assignment assignment;
 	private Schedule inviterSchedule;
 	private String nameOfClass;
-	
-	
-	
+
+
+
 	/**
 	 * @param inviter
 	 * @param possibleCollaborators
@@ -31,14 +29,14 @@ public class CollabInvite {
 		this.inviterSchedule = inviterSchedule;
 		this.nameOfClass = nameOfClass;
 	}
-	
+
 	public ArrayList<int[]> findNullTimes(Schedule inviterSchedule) {
 		Event[][][] schedule = inviterSchedule.getTempSchedule();
 		ArrayList<int[]> freeTimeSlots = new ArrayList<int[]>();
 		for (int i = 0; i < 13; i++) {
 	        for (int j = 0; j < 7; j++) {
 	            for (int k = 0; k < 24; k++) {
-	                if (schedule[i][j][k] == EventType.FREETIME) {
+	                if (schedule[i][j][k].getEventType().equals("FREETIME")) {
 	                	int[] temp = new int[3];
 	                	temp[0] = i;
 	                	temp[1] = j;
@@ -50,7 +48,7 @@ public class CollabInvite {
 	    }
 		return freeTimeSlots;
 	}
-	
+
 	public ArrayList<Invite> createInvite(ArrayList<int[]> freeTimeSlots, Assignment assignment, Student inviter, GregorianCalendar dueDate) {
 		ArrayList<Invite> newInvites = new ArrayList<Invite>();
 		for (int[] timeSlot : freeTimeSlots) {
@@ -59,9 +57,9 @@ public class CollabInvite {
 		}
 		return newInvites;
 	}
-	
+
 	public void sendInvite(String nameOfClass, ArrayList<Invite> invites, ArrayList<Student> possibleCollaborators) {
-		
+
 		for(Student invitee : possibleCollaborators) {
 			Schedule inviteeSchedule = invitee.getStudentSchedule();
 			Set<String> classSet = inviteeSchedule.getClassSet();
@@ -73,14 +71,14 @@ public class CollabInvite {
 					int i = timeSlot[0];
 					int j = timeSlot[1];
 					int k = timeSlot[2];
-					if (schedule[i][j][k] == EventType.FREETIME) {
+					if (schedule[i][j][k].getEventType().equals("FREETIME")) {
 						//what happens when they are invited to an event
 					}
 				}
 			}
 		}
 	}
-	
+
 	//if someone accepts
 	//make a new event study group and add it to their calendars
 }
